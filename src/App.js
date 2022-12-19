97d078e188ba1ede854a868ccf775149d05b46e1
swapnilkurane <kuraneskswapnilsk@gmail.com>
2022-12-19 13:58:35 +0530

    added totals

--- src/App.js
+++ src/App.js
@@ -2,34 +2,32 @@ import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
-  const [totPayable, setTotpayable] = useState(0)
-  const [totDue, setTotdue] = useState(0)
   const [data, setData] = useState([])
-  const [isShown, setIsShown] = useState(false);
 
 async function getData(){
   fetch('./data.json')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     setData(data);
   });
   }
+
+  let totPay = data.reduce(function(preAl, currAl){
+    return preAl + currAl.payableAmount;
+  },0);
+
+  let totDue = data.reduce(function(preAl, currAl){
+    return preAl + currAl.invoiceDetail.amountDue;
+  },0);
 
   useEffect(()=>{
     getData()
   },[])
 
-  console.log(">>thisdata", data)
-  // data.map((e)=>{
-  //   // setTotpayable(totPayable + e.payableAmount)  
-  //   setTotpayable((totPayable) => totPayable + 1)
-  // })
-
   return (
     <div>
-    <h1>DUSS DIGITAL INTERVIEW ASSESMENT</h1>
-    <table>
+    <h1 className='h1'>DUSS DIGITAL INTERVIEW ASSESMENT</h1>
+    <table id='t1' className='t1'>
     <tr>
       <th>ID</th>
       <th>COMPANY-Name</th>
@@ -42,15 +40,10 @@ async function getData(){
     {data.map((e, i)=>{
       return(
         <tr>
-          <th>{i}</th>
+          <th>{i+1}</th>
           <th>{e.companyName}</th>
           <th>{e.resourceAlias.length}</th>
-           {/* {isShown && (
-              <div>
-                {e.resourceAlias}
-              </div>
-            )} */}
-          <th className={e.paymentStatus == "Pending" ? 'bg-red' : 'bg-green'}>{e.paymentStatus}</th>
+          <th className={e.paymentStatus === "Pending" ? 'bg-red' : 'bg-green'}>{e.paymentStatus}</th>
           <th>{e.payableAmount}</th>
           <th>{e.invoiceDetail.amountDue}</th>
           <th>{<button>add</button>}</th>
@@ -59,8 +52,8 @@ async function getData(){
 
     })}    
     </table>
-    <h3>Total Payable Amount: {totPayable}</h3>
-    <h3>Total Dues Pending:{totDue}</h3>
+    <h3>Total Payable Amount: {totPay}</h3>
+    <h3>Total Due Amount Pending: {totDue}</h3>
     </div>
   );
 }
